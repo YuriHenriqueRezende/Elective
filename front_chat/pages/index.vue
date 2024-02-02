e23cafaf261713f6d4db5db68aca7c302c8283af
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-02-01 23:20:46 -0300

    Update index.vue

---
+++ front_chat/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+    let question = ref("")
+
+    const sendMessage = () => {
+        alert("Button was clicked: " + question.value);
+    }
+
+</script>
+
+
+<template>
+    <div>
+        <h1>ChatBot do Senai!</h1>
+        <label for ="">Type here your message!</label> <br><br>
+        <input type="text" v-model="question"> <br><br>
+        <button @click="sendMessage">Send</button>
+    </div>
+</template>
+
+<style scoped>
+    button{
+        background-color: black;
+        color: white;
+
+    }
+</style>
